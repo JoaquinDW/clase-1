2e4ff29f6c7bb09abca4a293155883a4380db21f
BalthasarDW <baltha@korl.co>
2024-07-11 00:50:50 -0300

    add

---
+++ Clase-1-Introduccion/homework/index.js
@@ -0,0 +1,56 @@
+// Si bien todos los ejercicios tienen una descripcion,
+// Algunos ejercicios estan mas detallados en el archivo homework.md
+// Ademas tienes material de apoyo en el archivo README.md
+
+export function imprimirMensaje(mensaje) {
+  // Escribe aquí el código para imprimir un mensaje en la consola
+  // La función debe recibir un mensaje como parámetro y mostrarlo en la consola
+}
+
+export function suma(a, b) {
+  // 'a' y 'b' son números, sumalos y devolve su valor
+  //tu codigo
+}
+
+export function resta(a, b) {
+  // 'a' y 'b' son números, restalos y devolve su valor
+  //tu codigo
+}
+
+export function multiplicacion(a, b) {
+  // 'a' y 'b' son números, multiplicarlos y devolve su valor
+  //tu codigo
+}
+
+export function iguales(a, b) {
+  // 'a' y 'b' son números, devolver si son iguales o no
+  //tu codigo
+}
+
+export function calcularAreaTriangulo(base, altura) {
+  // Escribe aquí el código para calcular el área de un triángulo
+  // La función debe recibir 'base' y 'altura' como parámetros y retornar el área calculada
+  // Psst: Como se calcula el area de un triangulo?
+}
+
+export function obtenerDatosUsuario() {
+  // Escribi aca el código para hacer una solicitud GET a una API de usuarios
+  // La URL de la API es 'https://jsonplaceholder.typicode.com/users/1'
+  // usa fetch para realizar la solicitud
+  // La función debe retornar una promesa que resuelva con los datos del usuario obtenidos
+  // o rechace con un mensaje de error en caso de fallo.
+  // Psst: No olvides que fetch retorna una promesa, ademas recuerda manejar los errores con .catch()
+}
+
+export function verificarEdad(edad) {
+  // Escribe aquí el código para verificar la edad de una persona
+  // Utiliza una estructura de control (if, switch) para imprimir un mensaje según la edad
+  // Ejemplo: "Puede entrar" si la edad es 18 o más, de lo contrario "No puede entrar"
+}
+
+export function manipularObjetoBasico() {
+  // Escribe aquí el código para manipular un objeto básico en JavaScript
+  // Define un objeto llamado 'producto' con propiedades como nombre, precio, cantidad, etc.
+  // Realiza operaciones como modificar el precio del producto, agregar una nueva propiedad, etc.
+  // La función debe retornar el objeto modificado
+}
---
+++ Clase-1-Introduccion/homework/test/index.test.js
@@ -0,0 +1,58 @@
+import { describe, expect, it, vi } from "vitest"
+import {
+  suma,
+  resta,
+  multiplicacion,
+  iguales,
+  calcularAreaTriangulo,
+  verificarEdad,
+} from ".."
+
+describe("Clase-1-Introduccion/homework", () => {
+  describe("suma", () => {
+    it("debería sumar dos números", () => {
+      expect(suma(1, 2)).toBe(3)
+      expect(suma(5, 5)).toBe(10)
+      expect(suma(10, 10)).toBe(20)
+    })
+  })
+
+  describe("resta", () => {
+    it("debería restar dos números", () => {
+      expect(resta(1, 2)).toBe(-1)
+      expect(resta(5, 5)).toBe(0)
+      expect(resta(10, 10)).toBe(0)
+    })
+  })
+
+  describe("multiplicacion", () => {
+    it("debería multiplicar dos números", () => {
+      expect(multiplicacion(1, 2)).toBe(2)
+      expect(multiplicacion(5, 5)).toBe(25)
+      expect(multiplicacion(10, 10)).toBe(100)
+    })
+  })
+
+  describe("iguales", () => {
+    it("debería verificar si dos números son iguales", () => {
+      expect(iguales(1, 2)).toBe(false)
+      expect(iguales(5, 5)).toBe(true)
+      expect(iguales(10, 10)).toBe(true)
+    })
+  })
+
+  describe("calcularAreaTriangulo", () => {
+    it("debería calcular el área de un triángulo", () => {
+      expect(calcularAreaTriangulo(1, 2)).toBe(1)
+      expect(calcularAreaTriangulo(5, 5)).toBe(12.5)
+      expect(calcularAreaTriangulo(10, 10)).toBe(50)
+    })
+  })
+  describe("VerificarEdad", () => {
+    it("debería verificar la edad de una persona", () => {
+      expect(verificarEdad(17)).toBe("No puede entrar")
+      expect(verificarEdad(18)).toBe("Puede entrar")
+      expect(verificarEdad(19)).toBe("Puede entrar")
+    })
+  })
+})
